f3fbb591d3cf82b837e271a058a6d9142de143cc
Ksenia Kozyreva <kseniia.cozyreva@yandex.ru>
2026-04-16 21:51:12 +0300

    Fixed

--- conftest.py
+++ conftest.py
@@ -8,6 +8,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import LoginLocators 
 
+# Класс страницы
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 15)
+
+    def login_as_user(self, email, password):
+        email_input = self.wait.until(EC.visibility_of_element_located(LoginLocators.EMAIL_INPUT))
+        email_input.send_keys(email)
+        self.driver.find_element(*LoginLocators.PASSWORD_INPUT).send_keys(password)
+        
+        login_button = self.wait.until(EC.element_to_be_clickable(LoginLocators.LOGIN_BUTTON_ON_LOGIN_PAGE))
+        self.driver.execute_script("arguments[0].click();", login_button)
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome", help="Выберите браузер: chrome или firefox")
 
@@ -23,7 +37,6 @@ def driver(request):
             browser = webdriver.Firefox(service=service)
         except Exception:
             browser = webdriver.Firefox() 
-
     else:
         raise pytest.UsageError("--browser должен быть chrome или firefox")
 
@@ -39,15 +52,11 @@ def base_url():
 
 @pytest.fixture
 def login_user(driver, base_url): 
-    wait = WebDriverWait(driver, 15)
     driver.get(f"{base_url}login")
-    email_input = wait.until(EC.visibility_of_element_located(LoginLocators.EMAIL_INPUT))
-    email_input.send_keys("ksenia_kozyreva_43_127@yandex.ru")
     
-    driver.find_element(*LoginLocators.PASSWORD_INPUT).send_keys("pass1234")
+    login_page = LoginPage(driver)
+    login_page.login_as_user("ksenia_kozyreva_43_127@yandex.ru", "pass1234")
     
-    login_button = wait.until(EC.element_to_be_clickable(LoginLocators.LOGIN_BUTTON_ON_LOGIN_PAGE))
-    driver.execute_script("arguments[0].click();", login_button)
+    WebDriverWait(driver, 15).until(EC.url_to_be(base_url))
     
-    wait.until(EC.url_to_be(base_url))
-    return wait
+    return driver  
--- tests/test_personal_cabinet.py
+++ tests/test_personal_cabinet.py
@@ -1,53 +1,71 @@
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import PersonalCabinetLocators
+
+# Класс страницы 
+class PersonalCabinetPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 15)
+
+    def click_acc_btn(self):
+        acc_btn = self.wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.ACCOUNT_BUTTON))
+        self.driver.execute_script("arguments[0].click();", acc_btn)
+
+    def click_const_btn(self):
+        const_btn = self.wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.CONSTRUCTOR_BUTTON))
+        self.driver.execute_script("arguments[0].click();", const_btn)
+
+    def click_logo_btn(self):
+        logo_btn = self.wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.LOGO))
+        self.driver.execute_script("arguments[0].click();", logo_btn)
+
+    def click_logout_btn(self):
+        logout_btn = self.wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.LOGOUT_BUTTON))
+        self.driver.execute_script("arguments[0].click();", logout_btn)
 
 class TestPersonalCabinet:
 
     # Переход в личный кабинет 
     def test_go_to_personal_cabinet(self, driver, login_user, base_url):
-        wait = login_user 
+        page = PersonalCabinetPage(driver)
         
-        acc_btn = wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.ACCOUNT_BUTTON))
-        driver.execute_script("arguments[0].click();", acc_btn)
+        page.click_acc_btn()
         
-        wait.until(EC.url_contains("/account/profile"))
+        WebDriverWait(driver, 10).until(EC.url_contains("/account/profile"))
         assert "/account/profile" in driver.current_url
 
     # Переход из личного кабинета в Конструктор
     def test_go_to_constructor_from_profile(self, driver, login_user, base_url):
-        wait = login_user
+        page = PersonalCabinetPage(driver)
         
-        driver.execute_script("arguments[0].click();", wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.ACCOUNT_BUTTON)))
-        wait.until(EC.url_contains("/account/profile"))
+        page.click_acc_btn()
+        WebDriverWait(driver, 10).until(EC.url_contains("/account/profile"))
         
-        const_btn = wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.CONSTRUCTOR_BUTTON))
-        driver.execute_script("arguments[0].click();", const_btn)
+        page.click_const_btn()
         
-        wait.until(EC.url_to_be(base_url))
+        WebDriverWait(driver, 10).until(EC.url_to_be(base_url))
         assert driver.current_url == base_url
 
-    #  Переход из личного кабинета по клику на Логотип
+    # Переход из личного кабинета по клику на Логотип
     def test_go_to_main_page_via_logo(self, driver, login_user, base_url):
-        wait = login_user
+        page = PersonalCabinetPage(driver)
         
-        driver.execute_script("arguments[0].click();", wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.ACCOUNT_BUTTON)))
-        wait.until(EC.url_contains("/account/profile"))
+        page.click_acc_btn()
+        WebDriverWait(driver, 10).until(EC.url_contains("/account/profile"))
         
-        logo_btn = wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.LOGO))
-        driver.execute_script("arguments[0].click();", logo_btn)
+        page.click_logo_btn()
         
-        wait.until(EC.url_to_be(base_url))
+        WebDriverWait(driver, 10).until(EC.url_to_be(base_url))
         assert driver.current_url == base_url
 
     # Выход из аккаунта
     def test_logout_from_personal_cabinet(self, driver, login_user):
-        wait = login_user
+        page = PersonalCabinetPage(driver)
         
-        driver.execute_script("arguments[0].click();", wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.ACCOUNT_BUTTON)))
+        page.click_acc_btn()
+        page.click_logout_btn()
         
-        logout_btn = wait.until(EC.element_to_be_clickable(PersonalCabinetLocators.LOGOUT_BUTTON))
-        driver.execute_script("arguments[0].click();", logout_btn)
-        
-        wait.until(EC.url_contains("/login"))
+        WebDriverWait(driver, 10).until(EC.url_contains("/login"))
         assert "/login" in driver.current_url
